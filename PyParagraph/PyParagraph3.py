b45bf8f1d6f648f7279500176556e4fe91846211
Anna Riehle <riehlea@gmail.com>
2018-03-05 20:25:26 -0500

    Create output files

--- PyParagraph/PyParagraph3.py
+++ PyParagraph/PyParagraph3.py
@@ -29,7 +29,18 @@ avg_word_length = word_length_sum / word_count
 sentence_count = len(sentences)
 avg_sentence_length = sentence_length_sum / sentence_count
 
-print(word_count)
-print(avg_word_length)
-print(sentence_count)
-print(avg_sentence_length)
+print("Paragraph Analysis")
+print("-------------------")
+print("Approximate Word Count: " + str(word_count))
+print("Approximate Sentence Count: " + str(sentence_count))
+print("Average Letter Count: " + str(avg_word_length))
+print("Average Sentence Length: " + str(avg_sentence_length))
+
+f = open('output/paragraph3_stats.txt','w')
+f.write("Paragraph Analysis\n")
+f.write("-------------------\n")
+f.write("Approximate Word Count: " + str(word_count) + "\n")
+f.write("Approximate Sentence Count: " + str(sentence_count) + "\n")
+f.write("Average Letter Count: " + str(avg_word_length) + "\n")
+f.write("Average Sentence Length: " + str(avg_sentence_length) + "\n")
+f.close()
